30c75e9a9035989d5283dfb9445ae4b208da51d2
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-05-17 09:38:34 +0000

    header footer

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'homes#top'
+  get "home/about", to: "homes#about"
+  
+  resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
+  resources :users, only: [:index, :show, :new, :edit, :update]
 end
